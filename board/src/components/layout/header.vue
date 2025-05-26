2fadc16bb0651e49e009b50f5906f5853958e32e
Fanssdy <fanssdy@qq.com>
2025-05-27 00:01:46 +0800

    header css

--- board/src/components/layout/header.vue
+++ board/src/components/layout/header.vue
@@ -1,44 +1,66 @@
 <script setup lang="ts">
 import { Search } from '@element-plus/icons-vue'
-import { reactive, ref } from 'vue'
+// import { reactive, ref } from 'vue'
 const SearchVisible = ref(false)
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
-
 <template>
-    <div class="navbar-wrapper">
-        <div class="header-container">
-            <div class="logo-container">
-                <img alt="Vue logo" class="logo" src="@/assets/logo.svg"     width="28" height="28"/>
-            </div>
-            <div class="content">
-                
-                <el-button @click="SearchVisible = true" plain size="large" :icon="Search" round>搜索</el-button>
-                
-                
-                <nav>
-                    <RouterLink to="/">Home</RouterLink>
-                    <RouterLink to="/about">About</RouterLink>
-                </nav>
-
-            </div>    
-        </div>
-    </div>
-    <el-dialog v-model="SearchVisible" title="Shipping address" width="500" :show-close="false">
-        <template #header="{  titleId, titleClass }">
-            <div class="my-header">
-                <h4 :id="titleId" :class="titleClass">搜索功能敬请期待</h4>
-            <!-- <el-button type="danger" @click="close">
-            <el-icon class="el-icon--left"><CircleCloseFilled /></el-icon>
-            Close
-            </el-button> -->
-            </div>
-    </template>
-    This is dialog content.
-    </el-dialog>
-
+    <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+    
+    
+  >
+    <el-menu-item index="0">
+      <img
+        style="width: 100px"
+        src="@/assets/logo.svg"
+        alt="logo"
+        width="28" height="28"
+      />
+    </el-menu-item>
+    <el-menu-item >
+        <el-button class="searchbutton" @click="SearchVisible = true" plain size="large" :icon="Search" round>
+        搜索
+        </el-button>
+    </el-menu-item>
+    
+    <el-menu-item index="1">Processing Center</el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>Workspace</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+  </el-menu>
 </template>
 
+
 <style scoped>
+.el-menu--horizontal > .el-menu-item:nth-child(1) {
+    margin-right: auto;
+}
+.el-menu--horizontal .el-menu-item:not(.is-disabled):focus,
+.el-menu--horizontal .el-menu-item:not(.is-disabled):hover {
+  background-color: var(--el-menu-bg-color);
+  color: var(--el-menu-active-color) !important;
+  outline: none !important;
+  border-bottom: 2px solid #1890ff; /* 可选：添加底部边框 */
+}
 .my-header {
   display: flex;
   flex-direction: row;
